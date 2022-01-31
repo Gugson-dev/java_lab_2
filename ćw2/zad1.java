5891067a8d28601d2ee54402ac629592f55308c5
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-01-31 23:10:42 +0100

    Zrobione
    Poprawiono tabulacje

--- ćw2/zad1.java
+++ ćw2/zad1.java
@@ -1,46 +1,51 @@
 import java.util.Scanner;
 
-public class zad1 {
+public class zad1 
+{
+  /*
+  
+  Zadanie praktycznie identyczne co ćw1/zadanie 1, dlatego dodałem metodę żeby kod się trochę różnił
 
-    /*
-    
-    Zadanie praktycznie identyczne co ćw1/zadanie 1, dlatego dodałem metodę żeby kod się trochę różnił
+  */
 
+  public static int skroc(int indeks, int ile)
+  {
+    String ost_cyfry = Integer.toString(indeks);
+    int podzielnik = Integer.parseInt(ost_cyfry.substring(ost_cyfry.length() - ile));
+    return podzielnik;
+  }
+
+  public static void main(String[] args) 
+  {
+    /* zad.1 Napisz program który będzie iterował od 100 do 0,
+    *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
+    * (jesli tą liczbą jest 0 to przez 2 ostatnie) 
     */
 
-    public static int skroc(int indeks, int ile){
+    Scanner input = new Scanner(System.in);
 
-        String ost_cyfry = Integer.toString(indeks);
-        int podzielnik = Integer.parseInt(ost_cyfry.substring(ost_cyfry.length() - ile));
-        return podzielnik;
+    System.out.print("Podaj swój numer indeksu: ");
+    int indeks = input.nextInt();
+    input.close();
 
-    }
+    int x = 100;
+    int ostatnia = skroc(indeks, 1);
+    int ostatnie2 = skroc(indeks, 2);
 
-    public static void main(String[] args) {
-        /* zad.1 Napisz program który będzie iterował od 100 do 0,
-         *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
-         * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Podaj swój numer indeksu: ");
-        int indeks = input.nextInt();
-        input.close();
-
-        int x = 100;
-        int ostatnia = skroc(indeks, 1);
-        int ostatnie2 = skroc(indeks, 2);
-
-        do {
-          if (ostatnia == 0) {
-              if (x % ostatnie2 == 0) {
-                System.out.printf("Liczba %d podzielna przez %d\n", x, ostatnie2);
-              }
-          }
-          else if (x % ostatnia == 0) {
-                System.out.printf("Liczba %d podzielna przez %d\n", x, ostatnia);
-          }
-          x--;
-        } while (x != 0);
-    }
+    do 
+    {
+      if (ostatnia == 0) 
+      {
+        if (x % ostatnie2 == 0) 
+        {
+          System.out.printf("Liczba %d podzielna przez %d\n", x, ostatnie2);
+        }
+      }
+      else if (x % ostatnia == 0) 
+      {
+        System.out.printf("Liczba %d podzielna przez %d\n", x, ostatnia);
+      }
+      x--;
+    } while (x != 0);
+  }
 }
--- ćw2/zad2.java
+++ ćw2/zad2.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
 
-public class zad2 {
+public class zad2 
+{
+   public static void main(String[] args) 
+   {
+      /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
+      * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
+      * za pomocą pętli forEach powiększone o 11 */
 
-    public static void main(String[] args) {
-        /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
-         * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
-         * za pomocą pętli forEach powiększone o 11 */
+      Scanner input = new Scanner(System.in);
+      int[] liczby = new int[5];
 
-         Scanner input = new Scanner(System.in);
-         int[] liczby = new int[5];
+      System.out.println("Podaj kolejno 5 liczb:");
+      for (int i = 0; i < 5; i++) 
+      {
+         System.out.printf("%d. ", i+1);
+         liczby[i] = input.nextInt();
+      }
 
-         System.out.println("Podaj kolejno 5 liczb:");
-         for (int i = 0; i < 5; i++) {
-            System.out.printf("%d. ", i+1);
-            liczby[i] = input.nextInt();
-         }
+      input.close();
+      int k = 1;
 
-         input.close();
-         int k = 1;
-
-         System.out.println("\nTwoje liczby zwiększone o 11 to:");
-         for (int l : liczby) {
-            System.out.printf("%d. %d\n", k, l+11);
-            k++;
-         }
-    }
+      System.out.println("\nTwoje liczby zwiększone o 11 to:");
+      for (int l : liczby) 
+      {
+         System.out.printf("%d. %d\n", k, l+11);
+         k++;
+      }
+   }
 }
